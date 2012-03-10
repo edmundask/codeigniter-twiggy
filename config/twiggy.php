59d0c3a393d696785c9277a1e3f1652649445927
Edmundas Kondrasovas <as@edmundask.lt>
2012-03-10 15:24:55 +0200

    Update to 0.8.1

--- config/twiggy.php
+++ config/twiggy.php
@@ -12,7 +12,7 @@
  * @category  			Config
  * @author    			Edmundas Kondrašovas <as@edmundask.lt>
  * @license   			http://www.opensource.org/licenses/MIT
- * @version   			0.8.0
+ * @version   			0.8.1
  * @copyright 			Copyright (c) 2012 Edmundas Kondrašovas <as@edmundask.lt>
  */
 
